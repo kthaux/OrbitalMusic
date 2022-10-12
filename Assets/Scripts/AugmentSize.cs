2377535142c8b08c625c8314d5e5f16bf5818844
Michelle Kim <mkim139@ucsc.edu>
2022-10-12 15:15:51 -0700

    right click resizing function

---
+++ Assets/Scripts/AugmentSize.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AugmentSize : MonoBehaviour
+{
+    //reference to the position of planet
+    public Transform Anchor; 
+    //start function
+    private float CameraZDistance;
+    private Vector3 initialScale;
+    private Camera mainCam;
+
+    private bool isMouseDragging;
+    private float min = 0.2f;
+    private float max = 10.0f;
+
+    private Vector3 MouseScreenPosition;
+    private Vector3 MouseWorldPosition;
+
+    private void Start(){
+        initialScale = transform.localScale;
+        mainCam = Camera.main;
+        CameraZDistance = mainCam.WorldToScreenPoint(transform.position).z;
+    }
+
+    
+    
+    private void OnMouseOver(){
+        if(Input.GetMouseButtonDown(1)){
+            GetComponent<Rotate>().enabled = false;
+            Debug.Log("right mouse button pressed");
+
+            isMouseDragging = true;
+        }
+    }
+    
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(Input.GetMouseButtonDown(1)){
+            //Debug.Log("right click");
+            // GetComponent<Rotate>().enabled = false;
+            isMouseDragging = true;
+            //scaleDistance = Vector3.Distance(initialScale, GetMouseWorldPosition());
+        }
+        */
+
+        if(Input.GetMouseButtonUp(1)){
+            Debug.Log("right click up");
+            isMouseDragging = false;
+            GetComponent<Rotate>().enabled = true;
+        }
+
+        if(isMouseDragging){
+
+            MouseScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, CameraZDistance);
+            MouseWorldPosition = mainCam.ScreenToWorldPoint(MouseScreenPosition);
+
+            Debug.Log("right click dragging");
+
+            // this is the code for resizing via world coordinates and not just y 
+            //float distance = Mathf.Clamp(Vector3.Distance(Anchor.position, MouseWorldPosition), min, max);
+            //float distance = Vector3.Distance(Anchor.position, MouseWorldPosition);
+            Debug.Log(MouseWorldPosition);
+
+            //the absolute value makes the size augmentable both ways
+            //take it off if you want one way resizing
+            float sizeScale = Mathf.Clamp(Mathf.Abs(MouseWorldPosition.y), min, max);
+
+
+            //this changes size
+            transform.localScale = new Vector3(sizeScale, sizeScale, sizeScale);
+
+        }
+    }
+}
--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -6,6 +6,7 @@ public class Draggable : MonoBehaviour
 {
     Vector3 mousePosOffset;
     private float mouseZCoordinate;
+    private Vector3 pos;
 
     //utility function to translate mouse postion to world position
     private Vector3 GetMouseWorldPosition(){
@@ -16,16 +17,18 @@ public class Draggable : MonoBehaviour
 
 
     private void OnMouseDown(){
-      mouseZCoordinate = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-      //captures mouse offset so player can click on any part of the planet body
-      mousePosOffset = gameObject.transform.position - GetMouseWorldPosition();
       GetComponent<Rotate>().enabled = false;
+        mouseZCoordinate = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        //captures mouse offset so player can click on any part of the planet body
+        mousePosOffset = gameObject.transform.position - GetMouseWorldPosition();
+        
     }
 
 
     private void OnMouseDrag(){
-      gameObject.transform.position = GetMouseWorldPosition() + mousePosOffset;
-      Debug.Log(gameObject.transform.position);
+      pos = GetMouseWorldPosition() + mousePosOffset;
+      pos.y = 0;
+      gameObject.transform.position = pos;
     }
 
     private void OnMouseUp(){
